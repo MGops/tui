ccfb0690eb38cf991c87e583b4f650e241e57aba
Mahesh Gopakumar <maheshgopakumar@gmail.com>
2024-09-22 18:40:24 +0100

    Add Tree
    added tree in sidebar

--- tui.py
+++ tui.py
@@ -1,17 +1,28 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Static, Button, Label, Input
+from textual.widgets import Header, Footer, Static, Button, Label, Tree
 from textual.widget import Widget
 from textual.containers import Vertical, Container
 
 
-class Sidebar(Widget):
+class PtList(Tree):
 
+    def compose(self):
+        #yield Tree("Patients")
+        tree: Tree[dict] = Tree("Patients")
+        tree.root.expand()
+        names = tree.root.add("Names", expand=True)
+        names.add("John Doe")
+        names.add("Jane Doe")
+        yield tree
+
+
+class Sidebar(Widget):
 
     CSS_PATH = 'tui.tcss'
 
-
     def compose(self):
         with Vertical():
+            yield PtList("Patients")
             yield Label ('Main Menu')
             #yield Input(placeholder="Initials")
             yield Button("Patient List")
